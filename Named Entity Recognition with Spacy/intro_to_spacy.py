85db560eeaa0041330af8cd7ef4464c838e9bc39
Krum Arnaudov <krum.arnaudov@amplifyanalytix.com>
2021-01-20 11:28:56 +0200

    added sentence splitting, difference between small and large spaCy model

--- Named Entity Recognition with Spacy/intro_to_spacy.py
+++ Named Entity Recognition with Spacy/intro_to_spacy.py
@@ -3,11 +3,30 @@ import spacy
 
 #Breaking into chapters
 with open("./data/alice.txt", 'r', encoding="utf-8") as f:
-    text = f.read()
+    text = f.read().replace("\n\n", " ").replace("\n", " ")
     chapters = text.split('CHAPTER ')[1:]
-    print(chapters[1])
+
+chapter1 = chapters[12]
+
+nlp = spacy.load('en_core_web_md')
+
+doc = nlp(chapter1)
+sentences = list(doc.sents)
 
 
-nlp = spacy.load('en_core_web_sm')
+sentence = sentences[0]
+print(sentence)
+ents = list(sentence.ents)
+print(ents)
+print(ents[1].label)
+print(ents[1].label_)
+print(ents[1].text)
 
-doc = nlp(chapters[0])
+people = []
+
+full_ents = list(doc.ents)
+for ent in full_ents:
+    if ent.label_ == "PERSON":
+        people.append(ent.text)
+
+print(set(people))
