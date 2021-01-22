df802f2a3fe9cd66395dc565554696a2fe551984
Krum Arnaudov <krum.arnaudov@amplifyanalytix.com>
2021-01-22 11:30:53 +0200

    played with textacy, extracted patterns of POS from text

--- Named Entity Recognition with Spacy/intro_to_spacy.py
+++ Named Entity Recognition with Spacy/intro_to_spacy.py
@@ -1,5 +1,6 @@
 ### Small intro to spaCy, following the Python for Digital Humanities tutorial
 import spacy
+import textacy
 
 #Breaking into chapters
 with open("./data/alice.txt", 'r', encoding="utf-8") as f:
@@ -15,18 +16,48 @@ sentences = list(doc.sents)
 
 
 sentence = sentences[0]
-print(sentence)
-ents = list(sentence.ents)
-print(ents)
-print(ents[1].label)
-print(ents[1].label_)
-print(ents[1].text)
+# print(sentence)
+# ents = list(sentence.ents)
+# print(ents)
+# print(ents[1].label)
+# print(ents[1].label_)
+# print(ents[1].text)
 
-people = []
+# people = []
 
-full_ents = list(doc.ents)
-for ent in full_ents:
-    if ent.label_ == "PERSON":
-        people.append(ent.text)
+# full_ents = list(doc.ents)
+# for ent in full_ents:
+#     if ent.label_ == "PERSON":
+#         people.append(ent.text)
 
-print(set(people))
+# print(set(people))
+
+# nouns = []
+# for token in sentence:
+#     if token.pos_ == 'NOUN':
+#         nouns.append(token)
+
+# print(nouns)
+
+## Noun chunks are groups that are linked to a noun "the book", "a very small cake", "New York City Police Station"
+# chunks = list(doc.noun_chunks)
+
+# for chunk in chunks:
+#     if "watch" in chunk.text:
+#         print(chunk)
+
+## Verbs - find patterns
+
+# patterns = [{"POS": 'ADV'}, {"POS":"VERB"}]
+
+# verb_phrases = textacy.extract.matches(doc, patterns=patterns)
+
+# for verb_phrase in verb_phrases:
+#     print(verb_phrase)
+
+patterns = [[{"POS": 'NOUN'}, {"POS":"VERB"}, {"POS": 'ADV'}], [{"POS": 'PRON'}, {"POS":"VERB"}, {"POS": 'ADV'}]]
+
+verb_phrases = textacy.extract.matches(doc, patterns=patterns)
+
+for verb_phrase in verb_phrases:
+    print(verb_phrase)
